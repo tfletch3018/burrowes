b9c1614a86eeadce2629224c1f40da181b107d46
Tonya Fletcher <tfletch4504@gmail.com>
2023-10-31 15:45:06 -0400

    add animation to home page

--- src/pages/bio.jsx
+++ src/pages/bio.jsx
@@ -29,7 +29,7 @@ function Bio() {
                     <Col><img id="profilePic" src={profile} alt="" /></Col>
                     <Col id="authorText">
                         <p>
-                        <i class="fa fa-quote-left"></i>
+                        <i className="fa fa-quote-left"></i>
                         &nbsp;My journey as a writer began soon after I got my first job. I went to work in the office of the Prime Minister. 
                         There I discovered a number of his speeches and, along with a colleague, I edited and submitted them to a 
                         London Publisher. They were published by Longman Green in 1970. In 1971, I came to the USA on a Fulbright scholarship 
@@ -40,7 +40,7 @@ function Bio() {
                         In 1980 I joined JP Morgan on Wall street and began my career as a banker in the USA. Over the years I worked for Dean Witter, 
                         Morgan Stanley and Calyon Financial. During this time, I maintained an interest in Caribbean politics and culture while working 
                         in banking. I have published two academic books on politics and, more recently, two books of travel essays.&nbsp;
-                        <i class="fa fa-quote-right"></i>
+                        <i className="fa fa-quote-right"></i>
                         </p>
                     </Col>
                 </Row>
